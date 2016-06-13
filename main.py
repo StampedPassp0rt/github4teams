db64c4158d34c5e6050e0577354bb96565ec054b
Ameet Rawtani <ameet.rawtani@gmail.com>
2016-06-13 11:39:35 -0400

    Added Paddle Class to paddle.py, created import of paddle and ball in main.py

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
 print "Somebody has to save our skin."
+
+from paddle import Paddle
+from ball import Ball
---
+++ paddle.py
@@ -0,0 +1,3 @@
+class Paddle():
+    def __init__(self):
+        self.hello = "Welcome to Pong!"
